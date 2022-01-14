5753eb6a0688fa522765b9c787c05ca2a280cfaf
Tim de Moor <timdemoor122@gmail.com>
2022-01-14 13:17:12 +0100

    updated tests

--- src/test/java/com/restaurantapp/RestaurantApp/Bestelling/BestellingControllerIntegrationTest.java
+++ src/test/java/com/restaurantapp/RestaurantApp/Bestelling/BestellingControllerIntegrationTest.java
@@ -5,7 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class BestellingControllerIntegrationTest {
@@ -13,15 +14,22 @@ public class BestellingControllerIntegrationTest {
     @Autowired
     private TestRestTemplate testRestTemplate;
 
-    //@Test
-    //public void getSingleTest(){
+    @Test
+    public void getSingleTest() {
 
-       //ResponseEntity<Bestelling> response = testRestTemplate.getForEntity("http://localhost:8080/api/v1/orders/1", Bestelling.class);
+        ResponseEntity<Bestelling> response = testRestTemplate.getForEntity("http://localhost:8080/api/v1/orders/1", Bestelling.class);
 
-        //assertFalse(response.getBody().isComplete);
-        //assertFalse(response.getBody().isComplete);
-        //assertFalse(response.getBody().isComplete);
-        //assertFalse(response.getBody().isComplete);
+        assertFalse(response.getBody().isComplete);
+        assertNotNull(response.getBody().id);
+        assertEquals(response.getBody().totalPrice, 0);
+    }
 
-    //}
+    @Test
+    public void getIncompleteTest(){
+        ResponseEntity<Bestelling> response = testRestTemplate.getForEntity("http://localhost:8080/api/v1/orders/incomplete", Bestelling.class);
+
+        assertFalse(response.getBody().isComplete);
+        assertNotNull(response.getBody().id);
+        assertEquals(response.getBody().totalPrice, 0);
+    }
 }
